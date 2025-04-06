789b7e6cdcb55af9b288d64700958914fe6ad8cf
MichaelVenturi <michaelventuri58@aol.com>
2025-04-06 16:03:35 -0400

    connect mongodb

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import connectDB from "./config/db";
 
-const PORT = 5000;
+config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
