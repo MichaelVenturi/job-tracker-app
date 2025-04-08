978cead1e4e87a4d16d1182771a0137fdac215a1
MichaelVenturi <michaelventuri58@aol.com>
2025-04-08 13:33:16 -0400

    boiler routes n pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,28 @@
-function App() {
-  return <h1 className="my-5 h-screen items-center">Hello</h1>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
